ad35b99c735db3d3c46263886354a6c8c9c6f5fb
Eveline Cynthia <pp.ksbwi@gmail.com>
2020-03-26 02:58:20 +0700

    Cek card regist

---
+++ _nuxt/cb7b22a85dd338771186.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{2813:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){this.$router.push({path:"/frontend/"})}},r=e(12),component=Object(r.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
